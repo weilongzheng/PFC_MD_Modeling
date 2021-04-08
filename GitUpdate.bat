ae7f774520247b10f88b3f7f38ce90d3b164a6b7
ZhongxuanWu <zhongxuanjessewu@gmail.com>
2021-04-08 10:48:00 +0800

    renamed GitUpdate.bat file

--- GitUpdate.bat
+++
@@ -1,5 +0,0 @@
-git add .
-git commit -m "created GitUpdate.bat file"
-git push
-git push
-exit
---
+++ gitpull.bat
@@ -0,0 +1,11 @@
+git add .
+git commit -m "renamed GitUpdate.bat file"
+git push
+git push
+git push
+git push
+git push
+git push
+git push
+git push
+exit
