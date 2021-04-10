6b05af3d68035700769723f4d22bcbb7c0431573
ZhongxuanWu <zhongxuanjessewu@gmail.com>
2021-04-10 13:20:58 +0800

    modified temp_model.py, added Elman_MD class

--- temp_model.py
+++ temp_model.py
@@ -812,9 +812,90 @@ class PytorchPFCMD(nn.Module):
 #output = pfc_md(input, target)
 #print(output.shape)
 
+class Elman_MD(nn.Module):
+    """Elman RNN with a MD layer
+    Parameters:
+    input_size: int, RNN input size
+    hidden_size: int, RNN hidden size
+    output_size: int, output layer size
+    num_layers: int, number of RNN layers
+    nonlinearity: str, 'tanh' or 'relu', nonlinearity in RNN layers
+    Num_MD: int, number of neurons in MD layer
+    num_active: int, number of active neurons in MD layer (refer to top K winner-take-all)
+    tsteps: int, length of a trial, equals to cuesteps + delaysteps
+    """
+
+    def __init__(self, input_size, hidden_size, output_size, num_layers=1, nonlinearity='tanh', Num_MD, num_active, tsteps, MDeffect=True):
+        super().__init__()
+
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.num_layers = num_layers
+        self.tsteps = tsteps
+
+        dt = 0.001 # Hard-coded for now
+
+        # Elman RNN layer
+        self.rnn = nn.RNN(input_size, hidden_size, num_layers, nonlinearity=nonlinearity)
+
+        # MD layer
+        self.MDeffect = MDeffect
+        if self.MDeffect:
+            self.md = MD(Nneur=Num_PFC, Num_MD=Num_MD, num_active=num_active, dt=dt)
+            #  initialize md_output
+            self.md_output = np.zeros(Num_MD)
+            index = np.random.permutation(Num_MD)
+            self.md_output[index[:num_active]] = 1 # randomly set part of md_output to 1
+            self.md_output_t = np.array([])
+
+        # Output layer
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, input, target):
+        n_time = input.shape[0]
+        batch_size = input.shape[1]
+
+        RNN_output = torch.zeros((n_time, batch_size, self.hidden_size))
+        RNN_hidden_t = torch.zeros((self.num_layers, batch_size, self.hidden_size))
+
+        for t in range(n_time):
+            input_t = input[t, ...].unsqueeze(dim=0)
+            target_t = target[t, ...].unsqueeze(dim=0)
+            
+            # Reinit MD activity for each trial
+            if t % self.tsteps == 0: 
+                if self.MDeffect:
+                    self.md.init_activity()  # Reinit MD activity
+
+            # TODO: integrate MD into Elman_MD
+            if self.MDeffect:
+                self.md_output = self.md(RNN_hidden_t.detach().numpy()[0, 0, :]) # batch size should be 1
+
+                self.md.MD2PFCMult = np.dot(self.md.wMD2PFCMult, self.md_output)
+                rec_inp = np.dot(self.rnn.weight_hh_l[k].detach().numpy(), RNN_hidden_t.detach().numpy())
+                md2pfc_weights = (self.md.MD2PFCMult / np.round(self.md.Num_MD / self.num_output))
+                md2pfc = md2pfc_weights * rec_inp  
+                md2pfc += np.dot(self.md.wMD2PFC / np.round(self.md.Num_MD /self.num_output), self.md_output) 
+                
+                RNN_hidden_t = self.pfc(input2pfc,torch.from_numpy(md2pfc))
+                pfc_output_t = RNN_hidden_t.view(1,RNN_hidden_t.shape[0])
+                self.pfc_outputs[i, :] = pfc_output_t
+
+                if i==0:
+                    self.md_output_t = self.md_output.reshape((1,self.md_output.shape[0]))
+                else:
+                    self.md_output_t = np.concatenate((self.md_output_t, self.md_output.reshape((1,self.md_output.shape[0]))),axis=0)
+
+            else:
+                RNN_output[t, :, :], RNN_hidden_t = self.rnn(input_t, RNN_hidden_t)
+
+        model_out = self.fc(RNN_output)
+        return model_out
+
 
 class LSTM_MD(nn.Module):
-    """LSTM with a MD layer (this models works only when batch_size = 1)
+    """LSTM with a MD layer (this models works only when MDeffect = False)
     Parameters:
     input_size: int, LSTM input size
     hidden_size: int, LSTM hidden size
@@ -869,7 +950,8 @@ class LSTM_MD(nn.Module):
             if t % self.tsteps == 0: 
                 if self.MDeffect:
                     self.md.init_activity()  # Reinit MD activity
- 
+
+            # TODO: integrate MD into LSTM_MD
             if self.MDeffect:
                 self.md_output = self.md(LSTM_hidden_t.detach().numpy()[:, 0, :]) # batch size should be 1
 
