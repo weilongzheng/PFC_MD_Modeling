f9441ca9e06f08980f67af2e136844fa7c4bff9b
Zhongxuan <zhongxuanjessewu@gmail.com>
2021-07-25 19:54:57 +0800

    changed performance curve

--- tmp/developing_RNNMD.py
+++ tmp/developing_RNNMD.py
@@ -379,3 +379,27 @@ for env_id in range(len(tasks)):
     plt.tight_layout()
     # plt.savefig('./animation/'+'performance.png')
     plt.show()
+
+# Task performance with MD & no MD
+log_noMD = np.load('./files/'+'log_noMD.npy', allow_pickle=True).item()
+label_font = {'family':'Times New Roman','weight':'normal', 'size':20}
+title_font = {'family':'Times New Roman','weight':'normal', 'size':25}
+legend_font = {'family':'Times New Roman','weight':'normal', 'size':12}
+for env_id in range(len(tasks)):
+    plt.figure()
+    plt.plot(log_noMD['stamps'], log_noMD['act_perfs'][env_id], color='grey', label='$ MD- $')
+    plt.plot(log['stamps'], log['act_perfs'][env_id], color='red', label='$ MD+ $')
+    plt.fill_between(x=[   0, 4000] , y1=0.0, y2=1.01, facecolor='red', alpha=0.05)
+    plt.fill_between(x=[4000, 8000] , y1=0.0, y2=1.01, facecolor='green', alpha=0.05)
+    plt.fill_between(x=[8000, 12000], y1=0.0, y2=1.01, facecolor='red', alpha=0.05)
+    plt.legend(bbox_to_anchor = (1.25, 0.7), prop=legend_font)
+    plt.xlabel('Trials', fontdict=label_font)
+    plt.ylabel('Performance', fontdict=label_font)
+    plt.title('Task{:d}: '.format(env_id+1)+tasks[env_id], fontdict=title_font)
+    # plt.xticks(ticks=[i*500 - 1 for i in range(7)], labels=[i*500 for i in range(7)])
+    plt.xlim([0.0, None])
+    plt.ylim([0.0, 1.01])
+    plt.yticks([0.1*i for i in range(11)])
+    plt.tight_layout()
+    # plt.savefig('./animation/'+'performance.png')
+    plt.show()
