8c2f66d01f35c4a6d4ed17a21186810531e17467
Wei-Long Zheng <32958572+weilongzheng@users.noreply.github.com>
2021-08-23 23:31:38 -0400

    Update ewc_PFC_RikhyeTask.py

--- ewc_PFC_RikhyeTask.py
+++ ewc_PFC_RikhyeTask.py
@@ -33,11 +33,12 @@ class TrainingDataset(Dataset):
             input = self.input[idx]
             return input,output
 
-RNGSEED_range = [1,2,3,4,5,6,7,8,9,10]
-for RNGSEED in RNGSEED_range:
+#RNGSEED_range = [1,2,3,4,5,6,7,8,9,10]
+weight_range = [0,10e10]
+for weight in weight_range:
     
     # Generate trainset
-    #RNGSEED = 1 # set random seed
+    RNGSEED = 1 # set random seed
     np.random.seed([RNGSEED])
     torch.manual_seed(RNGSEED)
     
@@ -68,7 +69,7 @@ for RNGSEED in RNGSEED_range:
     
     # Training
     criterion = nn.MSELoss()
-    ewc = ElasticWeightConsolidation(model, crit=criterion, lr=1e-3)
+    ewc = ElasticWeightConsolidation(model, crit=criterion, lr=1e-3, weight=weight)
     training_params = list()
     for name, param in model.named_parameters():
         print(name)
@@ -162,7 +163,7 @@ for RNGSEED in RNGSEED_range:
     
     filename = Path('files')
     os.makedirs(filename, exist_ok=True)
-    file_training = 'train_ewc_numMD'+str(Num_MD)+'_numContext'+str(Ncontexts)+'_MD'+str(MDeffect)+'_PFC'+str(PFClearn)+'_R'+str(RNGSEED)+'.pkl'
+    file_training = 'train_ewc'+str(weight)+'_numMD'+str(Num_MD)+'_numContext'+str(Ncontexts)+'_MD'+str(MDeffect)+'_PFC'+str(PFClearn)+'_R'+str(RNGSEED)+'.pkl'
     if activity_record:
         with open(filename / file_training, 'wb') as f:
             pickle.dump({'log':log,'Ntrain':Ntrain,'Nextra':Nextra,'wPFC2MDs_all':wPFC2MDs_all,'wMD2PFCs_all':wMD2PFCs_all,'MDpreTraces_all':MDpreTraces_all,'PFCouts_all':PFCouts_all,'MDouts_all':MDouts_all}, f)
