635871a91e52eb84a899201e94b6459809e1edfd
Zhongxuan <zhongxuanjessewu@gmail.com>
2021-08-25 14:21:45 +0800

    minor changes

--- tmp/developing_RNNMD.py
+++ tmp/developing_RNNMD.py
@@ -228,27 +228,27 @@ for i in range(total_training_cycle):
             plt.plot(net.rnn.md.md_output_t[-1, :])
             plt.title('MD activities', fontdict=font)
             # Heatmap wPFC2MD
-            ax = plt.subplot(2, 2, 4)
-            ax = sns.heatmap(net.rnn.md.wPFC2MDdelta_mask, cmap='Reds')
-            ax.set_xticks([0, 255])
-            ax.set_xticklabels([1, 256], rotation=0)
-            ax.set_yticklabels([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], rotation=0)
-            ax.set_xlabel('PFC neuron index', fontdict=font)
-            ax.set_ylabel('MD neuron index', fontdict=font)
-            ax.set_title('wPFC2MD', fontdict=font)
-            cbar = ax.collections[0].colorbar
-            cbar.set_label('connection weight', fontdict=font)
-            ## Heatmap wMD2PFC
             # ax = plt.subplot(2, 2, 4)
-            # ax = sns.heatmap(net.rnn.md.wMD2PFC, cmap='Blues_r')
-            # ax.set_xticklabels([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], rotation=0)
-            # ax.set_yticks([0, 255])
-            # ax.set_yticklabels([1, 256], rotation=0)
-            # ax.set_xlabel('MD neuron index', fontdict=font)
-            # ax.set_ylabel('PFC neuron index', fontdict=font)
-            # ax.set_title('wMD2PFC', fontdict=font)
+            # ax = sns.heatmap(net.rnn.md.wPFC2MD, cmap='Reds')
+            # ax.set_xticks([0, 255])
+            # ax.set_xticklabels([1, 256], rotation=0)
+            # ax.set_yticklabels([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], rotation=0)
+            # ax.set_xlabel('PFC neuron index', fontdict=font)
+            # ax.set_ylabel('MD neuron index', fontdict=font)
+            # ax.set_title('wPFC2MD', fontdict=font)
             # cbar = ax.collections[0].colorbar
             # cbar.set_label('connection weight', fontdict=font)
+            ## Heatmap wMD2PFC
+            ax = plt.subplot(2, 2, 4)
+            ax = sns.heatmap(net.rnn.md.wMD2PFC, cmap='Blues_r')
+            ax.set_xticklabels([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], rotation=0)
+            ax.set_yticks([0, 255])
+            ax.set_yticklabels([1, 256], rotation=0)
+            ax.set_xlabel('MD neuron index', fontdict=font)
+            ax.set_ylabel('PFC neuron index', fontdict=font)
+            ax.set_title('wMD2PFC', fontdict=font)
+            cbar = ax.collections[0].colorbar
+            cbar.set_label('connection weight', fontdict=font)
             ## Heatmap wMD2PFCMult
             # font = {'family':'Times New Roman','weight':'normal', 'size':20}
             # ax = plt.subplot(2, 3, 6)
