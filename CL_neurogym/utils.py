a9bf2a7740bd66d592da39135fe40b76a6a8480c
Zhongxuan <zhongxuanjessewu@gmail.com>
2021-09-21 00:52:15 +0800

    all task pairs

--- CL_neurogym/utils.py
+++ CL_neurogym/utils.py
@@ -17,37 +17,37 @@ def get_task_seqs():
     Generate task sequences
     '''
     ## 1. all pairs
-    # num_tasks = 2
-    # tasks = ['yang19.dms-v0',
-    #          'yang19.dnms-v0',
-    #          'yang19.dmc-v0',
-    #          'yang19.dnmc-v0',
-    #          'yang19.dm1-v0',
-    #          'yang19.dm2-v0',
-    #          'yang19.ctxdm1-v0',
-    #          'yang19.ctxdm2-v0',
-    #          'yang19.multidm-v0',
-    #          'yang19.dlygo-v0',
-    #          'yang19.dlyanti-v0',
-    #          'yang19.go-v0',
-    #          'yang19.anti-v0',
-    #          'yang19.rtgo-v0',
-    #          'yang19.rtanti-v0']
-    # task_seqs = list(itertools.permutations(tasks, num_tasks))
-    # task_seqs = [val for val in task_seqs for i in range(num_tasks)]
+    num_tasks = 2
+    tasks = ['yang19.dms-v0',
+             'yang19.dnms-v0',
+             'yang19.dmc-v0',
+             'yang19.dnmc-v0',
+             'yang19.dm1-v0',
+             'yang19.dm2-v0',
+             'yang19.ctxdm1-v0',
+             'yang19.ctxdm2-v0',
+             'yang19.multidm-v0',
+             'yang19.dlygo-v0',
+             'yang19.dlyanti-v0',
+             'yang19.go-v0',
+             'yang19.anti-v0',
+             'yang19.rtgo-v0',
+             'yang19.rtanti-v0']
+    task_seqs = list(itertools.permutations(tasks, num_tasks))
+    task_seqs = [val for val in task_seqs for i in range(num_tasks)]
     ## 2. pairs from different task families
-    GoFamily = ['yang19.dlygo-v0', 'yang19.go-v0']
-    AntiFamily = ['yang19.dlyanti-v0', 'yang19.anti-v0']
-    DMFamily = ['yang19.dm1-v0', 'yang19.dm2-v0', 'yang19.ctxdm1-v0', 'yang19.ctxdm2-v0', 'yang19.multidm-v0']
-    MatchFamily = ['yang19.dms-v0', 'yang19.dmc-v0', 'yang19.dnms-v0', 'yang19.dnmc-v0']
+    # GoFamily = ['yang19.dlygo-v0', 'yang19.go-v0']
+    # AntiFamily = ['yang19.dlyanti-v0', 'yang19.anti-v0']
+    # DMFamily = ['yang19.dm1-v0', 'yang19.dm2-v0', 'yang19.ctxdm1-v0', 'yang19.ctxdm2-v0', 'yang19.multidm-v0']
+    # MatchFamily = ['yang19.dms-v0', 'yang19.dmc-v0', 'yang19.dnms-v0', 'yang19.dnmc-v0']
     ### 2.1 two tasks
-    TaskA = GoFamily + AntiFamily
-    TaskB = MatchFamily + ['yang19.ctxdm1-v0', 'yang19.dm2-v0']
-    task_seqs = []
-    for a in TaskA:
-        for b in TaskB:
-            task_seqs.append((a, b))
-            task_seqs.append((b, a))
+    # TaskA = GoFamily + AntiFamily
+    # TaskB = MatchFamily + ['yang19.ctxdm1-v0', 'yang19.dm2-v0']
+    # task_seqs = []
+    # for a in TaskA:
+    #     for b in TaskB:
+    #         task_seqs.append((a, b))
+    #         task_seqs.append((b, a))
     ### 2.2 three tasks
     # TaskA = GoFamily + AntiFamily
     # TaskB = ['yang19.dm2-v0', 'yang19.ctxdm1-v0', 'yang19.multidm-v0']
